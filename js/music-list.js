6cbd6993ae7c317536d354beab63d5b0ab7a2827
mostafa <tefamod22@gmail.com>
2022-09-20 21:45:03 +0200

    music player

---
+++ js/music-list.js
@@ -0,0 +1,35 @@
+let allMusic = [
+{
+    name: "Believer ",
+    artist:"imagine Dragons",
+    img: "Believer",
+    src: "Believer"
+},
+
+{
+ name: "brazil",
+ artist:"afroto & marwan mousa",
+ img: "brazil",
+ src: "brazil"
+},
+{
+    name: " wasa3 wasa3 ",
+    artist:"ahmed saad",
+    img: "wasa3",
+    src: "wasa3" 
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+]
